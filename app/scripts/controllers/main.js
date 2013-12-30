3dfd31f0f9dff7a8a84e265d03fad22bfc0f972b
Jay Earwood <jay.earwood@gmail.com>
2013-12-30 15:13:21 -0600

    scaffolding out gumby framework

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,25 @@
 
 angular.module('promotracApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  	Gumby.init();
+
+  	$scope.dashboard = {
+  		title: "PromoTrac",
+  		body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, voluptatibus velit necessitatibus cum quisquam ducimus nam ab tempore rerum. A, necessitatibus et voluptatibus adipisci delectus facere optio corporis ipsa eligendi."
+  	};
+
+  	$scope.warehouse = {
+  		title: "Warehouse | Promotrac",
+  		body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, cumque eaque quisquam quibusdam facere laudantium beatae cum ex excepturi quas quo quis provident deleniti itaque facilis harum ipsam at aliquam!"
+  	};
+
+  	$scope.orders = {
+  		title: "Orders | Promotrac",
+  		body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, inventore, perspiciatis vero saepe magni nostrum sunt nulla ducimus consectetur sit cum possimus nemo quo corporis provident repellat numquam esse recusandae."
+  	};
+
+  	$scope.account = {
+  		title: "Account | Promotrac",
+  		body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, eveniet nisi unde quas excepturi voluptatem repellat deleniti quibusdam. Deserunt magni quam ea accusamus ipsa culpa beatae inventore doloremque eveniet aperiam!"
+  	};
   });
